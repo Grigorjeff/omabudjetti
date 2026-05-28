ef070b175d5c4408e0e997997cd4b5155356de53
Ivan Grigoryev <ivan@grigoryev.fi>
2026-05-28 08:37:42 +0300

    lisää AddItem- ja ItemForm-komponentit sekä reitityksen

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -1,16 +1,30 @@
 import styles from './Items.module.scss'
 import { FloatingButton } from '../../shared/ui/buttons'
 import Item from '../../components/Item'
-import { useLoaderData } from 'react-router'
+import { Link, useLoaderData } from 'react-router'
 
-
+/**
+ * Items-näkymäkomponentti.
+ *
+ * Hakee item-datan React Routerin loaderin kautta ja
+ * renderöi sen Item-komponentteina listaksi.
+ * Näkymä sisältää myös kelluvan toimintopainikkeen.
+ *
+ * @returns {JSX.Element}
+ */
 function Items() {
-const data = useLoaderData()
-const items = data.map(item => <Item key={item.id} data={item} />)
+
+  // Haetaan loader-funktion palauttama data.
+  const data = useLoaderData()
+
+  // Muodostetaan Item-komponenttien lista datasta,
+  // jokaiselle Itemille annetaan yksilöllinen key.
+  const items = data.map(item => <Item key={item.id} data={item} />)
+
   return (
     <div className={styles.items}>
       { items }
-      <FloatingButton secondary>+</FloatingButton>
+      <Link to="/add" viewTransition><FloatingButton secondary>+</FloatingButton></Link>
     </div>
   )
 }
--- src/router/AppRouter/AppRouter.jsx
+++ src/router/AppRouter/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import AddItem from '../../pages/AddItem'
 import ErrorPage from '../../pages/ErrorPage'
 import Items from '../../pages/Items'
 import MainLayout from '../../layout/MainLayout'
@@ -6,21 +7,21 @@ import Settings from '../../pages/Settings'
 import Stats from '../../pages/Stats'
 
 function AppRouter(props) {
-    const router = createBrowserRouter([
+  const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
       errorElement: <ErrorPage />,
       children: [
-        { path: "", 
+        { path: "",
           element: <Items />,
           loader: () => { return props.data } },
+        { path: "add", element: <AddItem /> },
         { path: "stats", element: <Stats /> },
         { path: "settings", element: <Settings /> }
       ]
     }
   ])
-
 
   return (
     <RouterProvider router={router} />
