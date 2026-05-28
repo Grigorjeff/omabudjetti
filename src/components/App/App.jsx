116388623a4ceb1888568951e71136f4e3128289
Ivan Grigoryev <ivan@grigoryev.fi>
2026-05-28 04:02:27 +0300

    lisää yleiset ulkoasumääritteet

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,20 @@
 import Item from '../Item'
-import './App.css'
+import styles from './App.module.scss'
+
 
 function App() {
 
-  return (
+    return (
     <>
-      <div>
-                <Item />
+      <div className={styles.app}>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
       </div>
     </>
   )
+
 }
 
 export default App
