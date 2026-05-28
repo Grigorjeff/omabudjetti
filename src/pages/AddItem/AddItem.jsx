ac14533ad8a1889d8607ee47d0de442792ec23f3
Ivan Grigoryev <ivan@grigoryev.fi>
2026-05-28 09:29:54 +0300

    lisää lomaketietojen tallennuksen

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -4,9 +4,9 @@ import styles from './AddItem.module.scss'
 function AddItem(props) {
 
   return (
-        <div className={styles.additem}>
+    <div className={styles.additem}>
       <h2>Uuden merkinnän lisääminen</h2>
-      <ItemForm />
+      <ItemForm onItemSubmit={props.onItemSubmit} />
     </div>
   )
 
